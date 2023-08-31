cce0795a3fa9fa3add92695acde5af56b37be59e
Beatriz Carvalheira <bcarvalheira@resideadmissions.com>
2023-08-31 12:06:55 -0300

    Stock Ratings - add test version of CNBC scraping

--- main.py
+++ main.py
@@ -1,73 +1,78 @@
+import time
+
 import pandas as pd
 from modules.navigation import *
 from modules.scraper import *
 from modules.output import *
 from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 from config.settings import *
-import requests
-from bs4 import BeautifulSoup
-stock_list = ["aapl",
-"abnb",
-"ael-a",
-"all-i",
-"amd",
-"amzn",
-"apo"]
+
+stock_list = ["AAPL",
+"ABNB",
+"AMD",
+"AMZN",
+"APO",
+"crwd",
+"csco",
+"ctra",
+              ]
 classification_terms = ['buy', 'sell', 'hold']
 
-status_list = []
-price_list = []
+rating_list = []
+price_target_list = []
+driver, success = make_request('https://www.cnbc.com/quotes/AAPL?qsearchterm=apple')
+if success:
+    driver.maximize_window()
+    time.sleep(1)
+    load_and_click(driver, '//*[@id="QuotePage-ICBanner"]/div/div/div[1]')
 
-for s in stock_list:
-    driver, success = make_request(f'https://www.tipranks.com/stocks/{s}/forecast')
-    status = 'N/A'
-    print(s)
-    if success:
-        driver.maximize_window()
-        scroll_script = "window.scrollBy(0, 300);"
-        driver.execute_script(scroll_script)
-        time.sleep(0.5)
+
+    # login and password
+    username = 'rameshsingh360@gmail.com'
+    password = '-lioN2o88'
+    insert_text(driver, '//*[@id="sign-in"]/div[1]/div/div/input', username)
+    insert_text(driver, '//*[@id="sign-in"]/div[2]/div/div/input', password)
+    load_and_click(driver, '//*[@id="sign-in"]/button[1]')
+    time.sleep(1)
+
+    # search the stock list
+    for s in stock_list:
+        print(s)
+        driver.get(f'https://www.cnbc.com/quotes/{s}')
+        time.sleep(3)
         soup = get_source(driver)
-
-        classification = find(soup, 'span', 'class', 'colorpale fonth4_bold aligncenter mobile_mb0 mobile_fontSize3small w12')
-        if classification:
-            classification_formatted = (classification.text).lower()
-
-            for c in classification_terms:
-                if c in classification_formatted:
-                    status = c
-                    break
-                else:
-                    status = 'N/A'
-            status_list.append(status)
-            content_div = soup.find('span', class_='fontWeightsemibold colorgray-1')
-            if content_div:
-                price = content_div.get_text()
-            else:
-                price = 'N/A'
-            price_list.append(price)
+        rating_block = find_all(soup, 'div', 'class', 'ICBanner-firstRow')
+        if len(rating_block)>0:
+            rating = rating_block[1].text
+            rating_list.append(rating)
         else:
-            status_list.append('N/A')
-            price_list.append('N/A')
-            continue
-    driver.quit()
+            rating_list.append('N/A')
+        price_target_block = find_all(soup, 'div', 'class', 'ICBanner-rowValue')
+        if len(price_target_block)>0:
+            price_target = price_target_block[0].text
+            price_target_list.append(price_target)
+        else:
+            price_target_list.append('N/A')
+driver.quit()
 
-if len(status_list)>0 and len(price_list)>0:
-    script_dir = os.path.dirname(sys.argv[0])
-
-    print(script_dir)
+if len(rating_list)>0 and len(price_target_list)>0:
     data = {
         "Name": stock_list,
-        "Rating": status_list,
-        "Average price": price_list
+        "Rating": rating_list,
+        "Price Target": price_target_list
     }
     df = pd.DataFrame(data)
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
     # Save the DataFrame to an Excel file
-    excel_filename = os.path.join(script_dir, f'tipranks_{timestamp}.xlsx')
-    df.to_excel(excel_filename, index=False)
+    script_dir = os.path.dirname(sys.argv[0])
+    print(script_dir)
+    excel_filename = f'tipranks_{timestamp}.xlsx'
+    df.to_excel(os.path.join(script_dir, excel_filename), index=False)
+
 
 
 
