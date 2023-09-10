5e29555a433bbdc7d4955b8e53ee347d7418dbf7
Beatriz Carvalheira <bcarvalheira@resideadmissions.com>
2023-09-10 18:00:56 -0300

    Stock Ratings - Add complete xlsx file reading function

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import time
-
 import pandas as pd
 from modules.navigation import *
 from modules.scraper import *
 from modules.output import *
 from datetime import datetime
 from selenium.webdriver.common.action_chains import ActionChains
-
-
 from config.settings import *
+
+# set current file directory (main)
+script_path = sys.argv[0]
+script_directory = os.path.abspath(os.path.dirname(script_path))
+
+first_column_values = get_first_column_values_from_xlsx(script_directory)
+first_column_values_definitive = []
+
+if first_column_values is not None:
+    print('## List Successfully found ##')
+    filtered_list = [item for item in first_column_values if item is not None]
+    for i in filtered_list:
+        first_column_values_definitive.append(i.upper())
+
+else:
+    print("No XLSX file found in the specified directory.")
+
+print(first_column_values_definitive)
+
+
+
+
+
 
 stock_list = ["AAPL",
 "ABNB",
@@ -19,62 +39,6 @@ stock_list = ["AAPL",
 "csco",
 "ctra",
               ]
-classification_terms = ['buy', 'sell', 'hold']
-
-rating_list = []
-price_target_list = []
-driver, success = make_request('https://www.cnbc.com/quotes/AAPL?qsearchterm=apple')
-if success:
-    driver.maximize_window()
-    time.sleep(1)
-    load_and_click(driver, '//*[@id="QuotePage-ICBanner"]/div/div/div[1]')
-
-
-    # login and password
-    username = 'rameshsingh360@gmail.com'
-    password = '-lioN2o88'
-    insert_text(driver, '//*[@id="sign-in"]/div[1]/div/div/input', username)
-    insert_text(driver, '//*[@id="sign-in"]/div[2]/div/div/input', password)
-    load_and_click(driver, '//*[@id="sign-in"]/button[1]')
-    time.sleep(1)
-
-    # search the stock list
-    for s in stock_list:
-        print(s)
-        driver.get(f'https://www.cnbc.com/quotes/{s}')
-        time.sleep(3)
-        soup = get_source(driver)
-        rating_block = find_all(soup, 'div', 'class', 'ICBanner-firstRow')
-        if len(rating_block)>0:
-            rating = rating_block[1].text
-            rating_list.append(rating)
-        else:
-            rating_list.append('N/A')
-        price_target_block = find_all(soup, 'div', 'class', 'ICBanner-rowValue')
-        if len(price_target_block)>0:
-            price_target = price_target_block[0].text
-            price_target_list.append(price_target)
-        else:
-            price_target_list.append('N/A')
-driver.quit()
-
-if len(rating_list)>0 and len(price_target_list)>0:
-    data = {
-        "Name": stock_list,
-        "Rating": rating_list,
-        "Price Target": price_target_list
-    }
-    df = pd.DataFrame(data)
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-    # Save the DataFrame to an Excel file
-    script_dir = os.path.dirname(sys.argv[0])
-    print(script_dir)
-    excel_filename = f'tipranks_{timestamp}.xlsx'
-    df.to_excel(os.path.join(script_dir, excel_filename), index=False)
-
-
-
 
 
 
