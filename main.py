ae44d389c7169cae21a63d148d6cc72d97f6a9d8
Beatriz Carvalheira <bcarvalheira@resideadmissions.com>
2023-09-13 13:10:42 -0300

    Stock Ratings - revert to xlsx processing only

--- main.py
+++ main.py
@@ -1,80 +1,105 @@
+import sys
 import time
-
+import webbrowser
 import pandas as pd
 from modules.navigation import *
 from modules.scraper import *
 from modules.output import *
 from datetime import datetime
 from selenium.webdriver.common.action_chains import ActionChains
-
-
 from config.settings import *
+from flask import Flask, render_template, request
 
-stock_list = ["AAPL",
-"ABNB",
-"AMD",
-"AMZN",
-"APO",
-"crwd",
-"csco",
-"ctra",
-              ]
-classification_terms = ['buy', 'sell', 'hold']
+# Determine the script's directory (where main.py is located)
+script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-rating_list = []
-price_target_list = []
-driver, success = make_request('https://www.cnbc.com/quotes/AAPL?qsearchterm=apple')
-if success:
-    driver.maximize_window()
-    time.sleep(1)
-    load_and_click(driver, '//*[@id="QuotePage-ICBanner"]/div/div/div[1]')
+# Construct the absolute path to the 'templates' folder
+template_dir = os.path.join(script_dir, 'templates')
+
+# Initialize the Flask app with the explicit template folder
+app = Flask(__name__, template_folder=template_dir)
+
+print(template_dir)
+
+stop_server = False
+
+first_column_values_definitive = []
 
 
-    # login and password
-    username = 'rameshsingh360@gmail.com'
-    password = '-lioN2o88'
-    insert_text(driver, '//*[@id="sign-in"]/div[1]/div/div/input', username)
-    insert_text(driver, '//*[@id="sign-in"]/div[2]/div/div/input', password)
-    load_and_click(driver, '//*[@id="sign-in"]/button[1]')
-    time.sleep(1)
+@app.route('/')
+def index():
+    return render_template('login.html')
 
-    # search the stock list
-    for s in stock_list:
-        print(s)
-        driver.get(f'https://www.cnbc.com/quotes/{s}')
-        time.sleep(3)
-        soup = get_source(driver)
-        rating_block = find_all(soup, 'div', 'class', 'ICBanner-firstRow')
-        if len(rating_block)>0:
-            rating = rating_block[1].text
-            rating_list.append(rating)
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    try:
+        # Retrieve data from the submitted form
+        username = request.form['username']
+        password = request.form['password']
+
+        # Call the main_program function to start processing and pass the username and password as arguments
+        main_result = main_program(username, password)
+
+        return main_result  # Return the result of main_program
+
+    except Exception as e:
+        print(f"Error processing form: {e}")
+        return "An error occurred."
+
+
+def main_program(username, password):  # Accept username and password as arguments
+    try:
+        print(f"Username: {username}")
+        print(f"Password: {password}")
+
+        script_path = sys.argv[0]
+        script_directory = os.path.abspath(os.path.dirname(script_path))
+        first_column_values = get_first_column_values_from_xlsx(script_directory)
+        if first_column_values is not None:
+            print('## List Successfully found ##')
+            filtered_list = [item for item in first_column_values if item is not None]
+            for i in filtered_list:
+                first_column_values_definitive.append(i.upper())
+                print(i)
         else:
-            rating_list.append('N/A')
-        price_target_block = find_all(soup, 'div', 'class', 'ICBanner-rowValue')
-        if len(price_target_block)>0:
-            price_target = price_target_block[0].text
-            price_target_list.append(price_target)
-        else:
-            price_target_list.append('N/A')
-driver.quit()
+            print("No XLSX file found in the specified directory.")
 
-if len(rating_list)>0 and len(price_target_list)>0:
-    data = {
-        "Name": stock_list,
-        "Rating": rating_list,
-        "Price Target": price_target_list
-    }
-    df = pd.DataFrame(data)
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        return """
+                <html>
+                <head>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+                    <title>Processing Started</title>
+                </head>
+                <body>
+                    <h1>Processing Started</h1>
+                    <p>The script is currently running. You can stop it by clicking the button below:</p>
+                    <form method="POST" action="/stop_processing">
+                        <input type="submit" value="Stop Script">
+                    </form>
+                </body>
+                </html>
+                """
 
-    # Save the DataFrame to an Excel file
-    script_dir = os.path.dirname(sys.argv[0])
-    print(script_dir)
-    excel_filename = f'tipranks_{timestamp}.xlsx'
-    df.to_excel(os.path.join(script_dir, excel_filename), index=False)
+    except Exception as e:
+        print(f"Error processing XLSX file: {e}")
+        return "An error occurred during processing."
 
 
+@app.route('/stop_processing', methods=['POST'])
+def stop_processing():
+    global stop_server
+    stop_server = True  # Set the flag to stop the Flask server
+    return """<html> <head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>Program Stopped</title> </head> <body> <h1>Program Stopped</h1> <p>The script has 
+    been stopped. Please close this browser window, and the current terminal window and click the executable file 
+    again to run.</p> </body> </html>"""
 
 
-
-
+if __name__ == '__main__':
+    url = "http://127.0.0.1:5000"  # Change this URL to match your app's address
+    webbrowser.open(url)
+    app.run(debug=True)
+    # Check if the server should be stopped
+    if stop_server:
+        sys.exit(0)
